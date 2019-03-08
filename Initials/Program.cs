6cb618297a8275c7c2662c5f7553e1f9349da3e6
Jessica Wyatt <wyattjd1@sbcglobal.net>
2019-03-08 17:11:53 -0600

    1st draft

--- Initials/Program.cs
+++ Initials/Program.cs
@@ -6,9 +6,29 @@ namespace Initials
     {
         public static void Main(string[] args)
         {
+            Console.Write("What's your name? ");
+            string Name = Console.ReadLine();
+            Name = Name.ToUpper();
+            Name = Name.Trim();
+            Console.WriteLine(Initials(Name));
             Console.ReadLine();
-        }
 
-        // TODO: Create a method that takes in a full name and return the initials in all capital letters.
+        }
+        public static string Initials(string Name)
+        {
+            string splitName = Name;
+            string[] split = splitName.Split(' ');
+            foreach (string item in split)
+            {
+                item.Remove(1);
+                Console.WriteLine(item.Remove(1));
+            }
+                Console.ReadLine();
+                return "";
+            }
+
+
+            // TODO: Create a method that takes in a full name and return the initials in all capital letters.
+        }
     }
-}
+
